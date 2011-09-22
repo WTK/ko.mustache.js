4def969c29d26ee9ddab27c2863c4a2019ad44eb
Marcin Wtorkowski <marcin.wtorkowski@gmail.com>
2011-09-22 12:04:46 +0200

    Initial release.

---
+++ README.md
@@ -0,0 +1,60 @@
+#ko.mustache.js
+
+ko.mustache.js is a template engine for [knockoutjs](http://knockoutjs.com). It enables you to use [mustache.js](https://github.com/janl/mustache.js/) template library in place of jQuery.tmpl.
+
+Tested with knockout v1.2.1, jQuery 1.4.2 and mustache.js v0.4.0-dev.
+
+##Installation
+
+Embed following resources in head section of your page:
+
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+    <script type="text/javascript" src="/js/mustache.js"></script>
+    <script type="text/javascript" src="/js/knockout-1.2.1.js"></script>
+    <script type="text/javascript" src="/js/ko.mustache.js"></script>
+
+**Important** - set default template engine to be used by knockoutjs. Make sure you do that **before** of your custom knockout.js related code (like creating observables, applying bindings etc.).
+
+    // set ko.mustache.js as a template engine for knockout
+    ko.setTemplateEngine(new mustacheTemplateEngine());
+
+##Example
+
+    <html>
+        <head>
+            <title>ko.mustache.js example</title>
+            <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+            <script type="text/javascript" src="/js/mustache.js"></script>
+            <script type="text/javascript" src="/js/knockout-1.2.1.js"></script>
+            <script type="text/javascript" src="/js/ko.mustache.js"></script>
+            <script>
+                ko.setTemplateEngine(new mustacheTemplateEngine());
+            </script>
+        </head>
+        <body>
+            <!-- place for rendered template -->
+            <div data-bind='template: "personTemplate"'> </div>
+
+            <!-- mustache template -->
+            <script id='personTemplate' type='text/html'>
+                {{ name }} is {{ age }} y
+                <button data-bind='click: makeOlder'>Make</button>
+            </script>
+
+            <!-- knockout model and bindings -->
+            <script type='text/javascript'>
+                var viewModel = {
+                    name: 'Martin',
+                    age: ko.observable(78),
+                    makeOlder: function() {
+                        this.age(this.age() + 1);
+                    }
+                };
+                ko.applyBindings(viewModel);
+            </script>
+        </body>
+    </html>
+
+##Copyright
+
+Copyright (c) 2011 Marcin Wtorkowski. License: MIT (http://www.opensource.org/licenses/mit-license.php)
---
+++ ko.mustache.js
@@ -0,0 +1,77 @@
+/*!
+ * Mustache Template Engine for Knockout
+ * http://github.com/WTK/ko.mustache.js
+ *
+ * Copyright Marcin Wtorkowski
+ * License: MIT (http://www.opensource.org/licenses/mit-license.php)
+ */
+mustacheTemplateEngine = function () {
+    this.getTemplateNode = function (template) {
+        var templateNode = document.getElementById(template);
+        if (templateNode == null)
+            throw new Error("Cannot find template with ID=" + template);
+        return templateNode;
+    }
+
+    this.renderTemplate = function (templateId, data, options) {
+        /*
+         * Combine passed data with list of references with callbacks added previously by
+         * #createJavaScriptEvaluatorBlock
+         */
+        data = combineObjects(data, codeBlocks);       
+        // render with mustache (they return string so we have to parse it to list of nodes since knockout.js expects us to do so)
+        renderedMarkup = Mustache.to_html(this.getTemplateNode(templateId).text, data);
+        //since knockout js has jQuery as a dependency anyway, we can use it to parse that text into a document fragment using jQuery.clean().
+        return jQuery.clean([renderedMarkup], document);
+    }
+
+    this.isTemplateRewritten = function (templateId) {
+        return this.getTemplateNode(templateId).isRewritten === true;
+    }
+
+    this.rewriteTemplate = function (template, rewriterCallback) {
+        var templateNode = this.getTemplateNode(template);
+        var rewritten = rewriterCallback(templateNode.text);
+        
+        templateNode.text = rewritten;
+        templateNode.isRewritten = true;
+    }
+
+    /*
+     *  Since mustache.js doesn't support inline javascript into templates
+     *  and knockout.js assumes otherwise we have to return a named simple tag and
+     *  keep reference to the function under that name.
+     *  That named function gets then called by mustache.js while rendering.
+     *  It's **double evil** because not only using *eval* but also *with* without
+     *  any parameter parsing.
+     *
+     *  @TODO: Come up with a way of doing it without eval, probably a some kind of parser.
+     *  @TODO: Come up with a better way of storing references to functions, later on called
+     *  by mustache.
+     **/
+    this.createJavaScriptEvaluatorBlock = function (script) {
+        var codeBlockName = "_ko_mustache_block"+codeBlocksCount,
+            codeBlockTag = "{{{"+codeBlockName+"}}}";
+
+        // save reference to function to call when rendering template with mustache
+        codeBlocks[codeBlockName] = function() {
+            return eval("with(this) {"+script+"}");
+        };
+        // increase number of blocks kept
+        codeBlocksCount++;
+        
+        return codeBlockTag;
+    }
+
+    // privates to mustacheTemplateEngine
+    var codeBlocksCount = 0,
+        codeBlocks = {},
+        combineObjects = function(input, to_merge) {
+            for (name in to_merge) {
+                input[name] = to_merge[name];
+            }
+            return input;
+        };
+};
+mustacheTemplateEngine.version = '1.0.0'
+mustacheTemplateEngine.prototype = new ko.templateEngine();
